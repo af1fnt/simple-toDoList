c97b127f3763702106c5b38c7c00f0fc56c6c1e1
af1fnt <afifnaufalt@gmail.com>
2024-12-02 16:49:36 +0700

    initial database

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
+
+const prisma = new PrismaClient();
+
+
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
