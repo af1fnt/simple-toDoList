ad6a4460458bfb493d38bd8d0f51bc67e1a2a784
af1fnt <afifnaufalt@gmail.com>
2024-12-04 03:10:20 +0700

    delete line

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,10 +16,10 @@ const register = async (req, res) => {
     return res.status(400).json({ message: 'User already exists' });
   }
 
-  // Hash password
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Buat user baru
+
   const user = await prismaClient.user.create({
     data: {
       email,
