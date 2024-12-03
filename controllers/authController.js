f2ee048043a5d4e153a44f4c4b9446f0c8db90f9
af1fnt <afifnaufalt@gmail.com>
2024-12-04 03:14:02 +0700

    update controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
     return res.status(400).json({ message: 'Invalid credentials' });
   }
 
-  // Cek password
+
   const isValidPassword = await bcrypt.compare(password, user.password);
 
   if (!isValidPassword) {
