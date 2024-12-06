a078f315c49870113984aaceaab72b1f5031a9e7
af1fnt <afifnaufalt@gmail.com>
2024-12-06 21:48:10 +0700

    feat: add auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+import { verifyToken } from '../utils/jwtUtils.js';
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid or expired token' });
+  }
+};
